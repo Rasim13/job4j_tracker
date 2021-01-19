7b5c56b28a92132c0881d2fcff9e946247aad8db
Rasim <Rasim467@mail.ru>
2021-01-19 21:33:08 +0400

    7. Date. Отображение даты. [#423537]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,34 +1,37 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created;
 
+    public Item(LocalDateTime created){
+        this.created = LocalDateTime.now();
+    }
+    public LocalDateTime getCreated(){
+        return created;
+    }
     public Item(){
 
     }
-
     public Item(int id){
         this.id = id;
     }
-
     public Item(int id, String name){
         this.id = id;
         this.name = name;
     }
-
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
