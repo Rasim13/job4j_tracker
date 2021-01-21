a4b03deeec1eaa1ba2770d1428b1085d96af8d8c
Rasim <Rasim467@mail.ru>
2021-01-21 17:58:35 +0400

    3. Массивы и модели. [#423542]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -7,29 +7,23 @@ public class Library {
         Book physics = new Book("Mechanics", 5);
         Book chemistry = new Book("Organic", 3);
         Book book = new Book("Clean code", 8);
-        Book[] numberOf = new Book[4];
+        Book[] numberOf = new Book[5];
         numberOf[0] = history;
         numberOf[1] = literature;
         numberOf[2] = physics;
         numberOf[3] = chemistry;
+        numberOf[4] = book;
         for (int index = 0; index < numberOf.length; index++){
             Book bk = numberOf[index];
             System.out.println(bk.getName() + " - " + bk.getNumber());
         }
         System.out.println();
         Book tmp = numberOf[0];
-        numberOf[0] = numberOf[numberOf.length - 1];
-        numberOf[numberOf.length - 1] = tmp;
+        numberOf[0] = numberOf[3];
+        numberOf[3] = tmp;
         for (int index = 0; index < numberOf.length; index++){
             Book bk = numberOf[index];
             System.out.println(bk.getName() + " - " + bk.getNumber());
         }
-        System.out.println();
-        for (int index = 0; index < numberOf.length; index++){
-            Book bk = numberOf[index];
-            System.out.println(bk.getName() + " - " + bk.getNumber());
-        }
-
-
     }
 }
