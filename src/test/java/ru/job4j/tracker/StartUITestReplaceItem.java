0ef63681109b4b476120be4fed1b1566e1c7a35b
Rasim <Rasim467@mail.ru>
2021-01-28 17:45:17 +0400

    5.1. Тестирование. Подготовка данных. [#423479]

---
+++ src/test/java/ru/job4j/tracker/StartUITestReplaceItem.java
@@ -0,0 +1,33 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.nullValue;
+
+public class StartUITestReplaceItem {
+
+    @Test
+    public void WhenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "replaced item"};
+        StartUI.replaceItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+
+    @Test
+    public void WhenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item(1);
+        String[] answers = {
+                String.valueOf(item.getId()), null};
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        boolean deleted = tracker.delete(item.getId());
+        assertThat(deleted, is(nullValue()));
+    }
+}
