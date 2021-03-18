3377013fb6cef9197bbc1800d921da0cd5c03359
Rasim <Rasim467@mail.ru>
2021-03-18 14:57:18 +0400

    4. Сортировка номера [#423444]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        return extractNumber(left) - extractNumber(right);
+    }
+
+    public int extractNumber(String s){
+        String num = s.replaceAll("\\D", "");
+        return num.isEmpty() ? 0 : Integer.parseInt(num);
+    }
+}
