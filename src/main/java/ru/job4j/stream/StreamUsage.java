5a7e31f98758a113296f0c48e3e84c8e4917b161
Rasim <Rasim467@mail.ru>
2021-03-25 23:30:20 +0400

    0. Stream API [#423281]

---
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamUsage {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>(List.of(-2, 5, 8, -9, -7, 6));
+        List<Integer> positiveNumbers = numbers.stream()
+                .filter(list -> list >= 0)
+                .collect(Collectors.toList());
+        positiveNumbers.forEach(System.out::print);
+    }
+}
